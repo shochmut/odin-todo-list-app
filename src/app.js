e31df0ea2a6e0afb0cd30a4151541106c5504d41
shochmut <skyler.hochmuth@live.com>
2023-12-08 18:20:54 -0700

    Added Todo Display, Add New Todo Form
    Need to complete the functionality of the add todo form next. Then need to finalize functionality of edit todo, delete todo, and checkbox todo

--- src/app.js
+++ src/app.js
@@ -2,19 +2,23 @@ import './styles.css';
 import notebookEditIcon from './Assets/notebook-edit.svg'
 import plusCircle from './Assets/plus-circle.svg'
 import windowCloseIcon from './Assets/window-close.svg'
+import squareEditIcon from './Assets/square-edit-outline.svg'
+import trashCanIcon from './Assets/trash-can.svg'
+import plusBoxIcon from './Assets/plus-box.svg'
 
 export const todoLogic = (function() {
     //module containing all application logic
     let todos = [];
     let projects = [];
+    let currentProject = 'Todo';
 
-    function createTodo(title, description, dueDate, priority, state, notes, checklist, project = 'Todo') {
+    function createTodo(title, description, dueDate, priority, done=false, notes, checklist, project = 'Todo') {
         const newTodo = {
             title: title,
             description: description,
             dueDate: dueDate,
             priority: priority,
-            state: state,
+            done: done,
             notes: notes,
             checklist: checklist,
             project: project, 
@@ -32,12 +36,24 @@ export const todoLogic = (function() {
         return {newProject, projects};
     }
 
-    return{todos, projects, createTodo, createProject};
+    function setProject(project) { 
+        currentProject = project;
+    }
+
+    function getCurrentProject() {
+        return currentProject;
+    }
+
+
+
+
+
+    return{todos, projects, createTodo, createProject, setProject, getCurrentProject};
 })();
 
 todoLogic.createProject('Todo')
 todoLogic.createProject('Test Project')
-todoLogic.createTodo('Example Todo', 'This is my first todo item', '12/1/2023', 'high', 'not started', 'notes', 'blank')
+todoLogic.createTodo('Example Todo', 'This is my first todo item', '12/1/2023', 'high', false, 'notes', 'checklist')
 
 
 export const renderWebsite = (function() {
@@ -67,11 +83,15 @@ export const renderWebsite = (function() {
 
         const title = document.createElement('h1');
         title.classList.add('sidebar-item');
-        title.innerHTML = 'Test Sidebar';
+        title.innerHTML = 'Projects';
         sidebar.appendChild(title);
 
+        const projectContainer = document.createElement('div');
+        projectContainer.classList.add('project-container');
+        sidebar.appendChild(projectContainer);
+        
         //iterate through and add projects to sidebar
-        todoLogic.projects.forEach(function (item, index) {sidebar.appendChild(renderProject(item))});
+        renderProjects(projectContainer);
 
         //add new project button
         const addProjectButton = document.createElement('button');
@@ -92,15 +112,16 @@ export const renderWebsite = (function() {
         return sidebar;
     }
 
-    function renderProject(project) {
-        const display = document.createElement('div');
-        display.classList.add('project-container');
+    function renderProjects(projectContainer) {
+        projectContainer.replaceChildren();  // this clears out the projects first before rendering
+        todoLogic.projects.forEach(function (item) {projectContainer.appendChild(renderProject(item))});
+    }
 
+    function renderProject(project) {
         const title = document.createElement('li');
         title.classList.add('project-title');
         title.innerHTML = project.title;
-        display.appendChild(title);
-        return display
+        return title
     }
 
     function createWorkspace() {
@@ -110,9 +131,75 @@ export const renderWebsite = (function() {
         const title = document.createElement('h1');
         title.classList.add('workspace-item');
         title.innerHTML = 'Test Workspace';
-
         workspace.appendChild(title);
+
+        renderTodos(workspace);
+
+        // Render The Add Todo Button
+        const addTodoButton = document.createElement('input');
+        addTodoButton.type = 'image';
+        addTodoButton.id = 'add-todo-button';
+        addTodoButton.src = plusBoxIcon;
+
+        workspace.appendChild(addTodoButton)
+
         return workspace;
+    }
+
+    function renderTodos(workspace) {
+        workspace.replaceChildren();
+        todoLogic.todos.forEach(function (item) {workspace.appendChild(createTodoDisplay(item))});
+    }
+
+    function createTodoDisplay(item) {
+        const TodoContainer = document.createElement('div');
+        TodoContainer.classList.add('todo-container');
+        
+        const title = document.createElement('p');
+        title.classList.add('todo-title');
+        title.innerHTML = item.title;
+
+        const description = document.createElement('p');
+        description.classList.add('todo-description');
+        description.innerHTML = item.description;
+
+        const dueDate = document.createElement('p');
+        dueDate.classList.add('todo-duedate');
+        dueDate.innerHTML = item.dueDate;
+
+        const priority = document.createElement('p');
+        priority.classList.add('todo-priority');
+        priority.innerHTML - item.priority;
+
+        const done = document.createElement('input');
+        done.classList.add('todo-done');
+        done.type = 'checkbox'
+        // Set the Checkbox Initial State
+        if (item.done === true) {
+            done.checked = true;
+        } else {
+            done.checked = false
+        }
+
+        const editButton = document.createElement('input');
+        editButton.classList.add('todo-edit');
+        editButton.type = 'image';
+        editButton.src = squareEditIcon;
+
+        const deleteButton = document.createElement('input');
+        deleteButton.classList.add('todo-delete');
+        deleteButton.type = 'image';
+        deleteButton.src = trashCanIcon;
+
+        TodoContainer.appendChild(title);
+        TodoContainer.appendChild(description);
+        TodoContainer.appendChild(dueDate);
+        TodoContainer.appendChild(priority);
+        TodoContainer.appendChild(editButton);
+        TodoContainer.appendChild(deleteButton);
+        TodoContainer.appendChild(done);
+
+        return TodoContainer;
     }
 
     function createAddProjectForm() {
@@ -149,7 +236,67 @@ export const renderWebsite = (function() {
         return form;
     }
 
-    return {createHeader, createSidebar, createWorkspace, createAddProjectForm}
+    function createAddTodoForm() {
+        const form = document.createElement('form');
+        form.id = 'create-todo-form';
+        const header = document.createElement('div');
+        header.id = 'create-todo-form-header'
+        form.appendChild(header);
+        const title = document.createElement('h1');
+        title.innerHTML = 'Add New To Do';
+        header.appendChild(title);
+        const windowCloseButton = document.createElement('button');
+        windowCloseButton.classList.add('close-window-button')
+        const windowClose = new Image();
+        windowClose.src = windowCloseIcon;
+        windowClose.classList.add('close-window-icon');
+        windowCloseButton.appendChild(windowClose)
+        header.appendChild(windowCloseButton);
+        const titleInput = document.createElement('input');
+        titleInput.classList.add('create-todo-title');
+        titleInput.type = 'text';
+        titleInput.name = 'title'
+        titleInput.placeholder = 'Todo Title';
+        form.appendChild(titleInput);
+        const submitButton = document.createElement('input');
+        submitButton.id = 'create-todo-submit';
+        submitButton.type = 'submit';
+        submitButton.value = "Create Todo";
+        form.appendChild(submitButton);
+
+        const description = document.createElement('input');
+        description.classList.add('create-todo-description');
+        description.type = 'text';
+        description.name = 'description'
+        description.placeholder = "Description: ex) laundry, bills, etc.";
+        form.appendChild(description);
+
+        const dueDateContainer = document.createElement('label');
+        dueDateContainer.classList.add('create-todo-duedate-label');
+        dueDateContainer.innerHTML = 'Due Date: ';
+        const dueDate = document.createElement('input');
+        dueDate.classList.add('create-todo-duedate');
+        dueDate.type = 'date';
+        dueDate.name = 'duedate';
+        dueDateContainer.appendChild(dueDate);
+        form.appendChild(dueDateContainer);
+
+
+
+
+
+
+
+
+        //     priority: priority,
+        //     notes: notes,
+        //     checklist: checklist,
+        //     project: project, 
+
+        return form
+    }
+
+    return {createHeader, createSidebar, createWorkspace, createAddProjectForm, renderProjects, createAddTodoForm}
     
 })();
 
--- src/index.js
+++ src/index.js
@@ -9,11 +9,15 @@ function loadWebsite() {
     content.appendChild(renderWebsite.createSidebar());
     content.appendChild(renderWebsite.createWorkspace());
     content.appendChild(renderWebsite.createAddProjectForm());
+    content.appendChild(renderWebsite.createAddTodoForm());
 
-    //open add project form handler
+    // Set of DOM Selectors
     const openAddProjectForm = document.getElementById('addProjectButton');
     const addProjectForm = document.getElementById('createProjectForm');
     const closeWindowButton = document.querySelector('.close-window-button');
+    const addTodoButton = document.querySelector('#add-todo-button');
+    const addTodoForm = document.getElementById('create-todo-form');
+    const closeTodoWindowButton = document.querySelector('#create-todo-form .close-window-button');
 
     // Toggle Add Project Form Visibility
     openAddProjectForm.addEventListener('click', () => {
@@ -25,13 +29,32 @@ function loadWebsite() {
       e.preventDefault();
       addProjectForm.classList.toggle('activated');
     })
-
+  
     // Add Project Form Submission and Refresh Sidebar
     addProjectForm.addEventListener('submit', (e) => {
       e.preventDefault();
       todoLogic.createProject(project.value);
-      project.value = '';
-      content.appendChild(renderWebsite.createSidebar());
+      project.value = ''; // reset form
+      renderWebsite.renderProjects(document.querySelector('.project-container'));
+    })
+
+    // Add To Do Event Handler
+    addTodoButton.addEventListener('click', (e) => {
+      addTodoForm.classList.toggle('activated');
+    })
+
+    // Close Add Todo Form
+    closeTodoWindowButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      addTodoForm.classList.toggle('activated');
+    })
+
+    addTodoForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const data = new FormData(addTodoForm);
+      console.log(data)
+      todoLogic.createTodo(data.title, data.description, data.duedate);
+      console.log(todoLogic.todos)
     })
   }
   
